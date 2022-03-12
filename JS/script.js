560d52a69bc3ebd4178de8e75fe6207698976439
Martin <mkmartinoes@gmail.com>
2022-03-12 12:57:06 +0300

    st: Portfolio image description

--- JS/script.js
+++ JS/script.js
@@ -18,16 +18,27 @@ $(document).ready(() => {
 
     // Designing the pop up
 
+    if($("#p1").is(":visible")){
+        $('#p1').hide();
+    }
+
     $("#pic1").hover(()=> {
         $("#pic1").css({
             "position": "relative",
-        })
+
+        });
+
+        $("#p1").show();
+        
         $("#p1").css({
             "z-index" : "1000",
             "position" : "absolute",
-            "background-color" : "black",
+            "background-color" : "#e69900",
             "color" : "white",
-            'top' : "0"
-        })
-    })
+            'top' : "30px",
+            "left" : "20px",
+            "padding" : "20px",
+            "font-size" : "2.5vh"
+        });
+    });
 });
